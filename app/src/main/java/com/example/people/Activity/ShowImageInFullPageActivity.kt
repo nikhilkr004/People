305d24c502da27d2550f50fabc47c5de99b811c8
nkgam <nikhilabc860@gmail.com>
2024-09-19 19:31:53 +0530

    //commibit

--- app/src/main/java/com/example/people/Activity/ShowImageInFullPageActivity.kt
+++ app/src/main/java/com/example/people/Activity/ShowImageInFullPageActivity.kt
@@ -1,6 +1,7 @@
 package com.example.people.Activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +18,6 @@ class ShowImageInFullPageActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
     }
 }
